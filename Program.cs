16f98042be2643df57cff67c17175801483f3541
SELENAGROUP2023 <selenagroup2008@yandex.ru>
2023-02-09 21:19:27 +0700

    Решение

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Вывести на экран массив из 8 элементов, выбранных программой
+
+int[] getArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 8);
+    }
+    return array;
+}
+void printArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write($" {inArray[i]} ");
+    }
+}
+
+int[] array=getArray();
+
+Console.Clear();
+printArray(array);
+
+
+
+
